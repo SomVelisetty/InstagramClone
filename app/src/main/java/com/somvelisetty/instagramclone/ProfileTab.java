28a2ea9e33a1d56bc1c7c3fed24564486702bbc6
SomVelisetty <you@example.com>
2020-05-18 01:02:16 +0530

    Implemented Users Tab

--- app/src/main/java/com/somvelisetty/instagramclone/ProfileTab.java
+++ app/src/main/java/com/somvelisetty/instagramclone/ProfileTab.java
@@ -7,6 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -14,8 +22,12 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+    private EditText edtProfileNmae, edtBio, edtProfession, edtHobbie, edtFavSport;
+    private Button btnUpdateInfo;
+
     public ProfileTab() {
         // Required empty public constructor
+
     }
 
 
@@ -23,6 +35,58 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtProfileNmae = view.findViewById(R.id.edtProfileName);
+        edtBio = view.findViewById(R.id.edtBio);
+        edtProfession = view.findViewById(R.id.edtProfession);
+        edtHobbie = view.findViewById(R.id.edtHobbies);
+        edtFavSport = view.findViewById(R.id.edtFavSport);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateinfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if (parseUser.get("profileName") == null)
+        {
+            edtProfileNmae.setText("");
+            edtBio.setText("");
+            edtProfession.setText("");
+            edtHobbie.setText("");
+            edtFavSport.setText("");
+        } else {
+            edtProfileNmae.setText(parseUser.get("profileName") + "");
+            edtBio.setText(parseUser.get("profileBio") + "");
+            edtProfession.setText(parseUser.get("profileProfession") + "");
+            edtHobbie.setText(parseUser.get("profileHobbies") + "");
+            edtFavSport.setText(parseUser.get("proFavSport") + "");
+        }
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                parseUser.put("profileName", edtProfileNmae.getText().toString());
+                parseUser.put("profileBio", edtBio.getText().toString());
+                parseUser.put("profileProfession", edtProfession.getText().toString());
+                parseUser.put("profileHobbies", edtHobbie.getText().toString());
+                parseUser.put("proFavSport", edtFavSport.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null)
+                        {
+                            FancyToast.makeText(getContext(), "Info Updated", Toast.LENGTH_LONG, FancyToast.INFO,true).show();
+                        } else
+                        {
+                            FancyToast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG, FancyToast.ERROR,true).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/somvelisetty/instagramclone/UsersTab.java
+++ app/src/main/java/com/somvelisetty/instagramclone/UsersTab.java
@@ -7,12 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UsersTab extends Fragment {
+
+    private ListView listView;
+    private ArrayList mArrayList;
+    private ArrayAdapter mArrayAdapter;
 
     public UsersTab() {
         // Required empty public constructor
@@ -23,6 +38,38 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        mArrayList = new ArrayList();
+        mArrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_expandable_list_item_1);
+
+        final TextView txtLodingData = view.findViewById(R.id.txtLoading);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e == null)
+                {
+                    if (users.size() > 0)
+                    {
+                        for (ParseUser user : users)
+                        {
+                            mArrayList.add(user.getUsername());
+                        }
+
+                        listView.setAdapter(mArrayAdapter);
+                        txtLodingData.animate().alpha(0).setDuration(2000);
+                        listView.setVisibility(View.VISIBLE);
+
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 }
